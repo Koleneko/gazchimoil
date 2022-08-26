3ac58eb346922eeb1953b2050ed9e6f67c6cc5df
wed120 <wed120@mail.ru>
2022-08-26 14:05:32 +0300

    По кайфу

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#17B6C0"
+      }
+    },
   },
   plugins: [],
 };
