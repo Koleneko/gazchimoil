2449ff6a119778aaf1d6ded86397cdc6d8027ce3
wed120 <wed120@mail.ru>
2022-08-31 15:16:53 +0300

    dev v.0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,46 @@
-import type {NextPage} from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
-import {trpc} from "../utils/trpc";
-
-type TechnologyCardProps = {
-    name: string;
-    description: string;
-    documentation: string;
-};
+import { trpc } from "../utils/trpc";
+import { useEffect } from "react";
+import EmailLogo from "../components/icons/EmailLogo";
+import Email from "../../public/icons/Email.svg";
+import Phone from "../../public/icons/Phone.svg";
 
 const Home: NextPage = () => {
-    const hello = trpc.useQuery(["example.hello", {text: "from tRPC"}]);
+  return (
+    <>
+      <Head>
+        <title>ГазХимОил</title>
+        <meta name="description" content='ООО "ГазХимОил"' />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </Head>
 
-    return (
-        <>
-            <Head>
-                <title>ГазХимОил</title>
-                <meta name="description" content='ООО "ГазХимОил"'/>
-                <link rel="icon" href="/favicons/favicon.ico"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
-                <link rel="manifest" href="/favicons/site.webmanifest"/>
-            </Head>
-
-            <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-                    <h1>Hi</h1>
-            </main>
-        </>
-    );
+      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+        <h1>Hi</h1>
+        {/*<EmailLogo />*/}
+        <Email />
+        <Phone />
+      </main>
+    </>
+  );
 };
 
 export default Home;
